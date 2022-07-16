68f262b1098493964dc0ea2df26e5abc82d3b9e5
OiiBertt <108538131+OiiBertt@users.noreply.github.com>
2022-07-16 17:34:21 +0700

    Create donasi.js

---
+++ plugins/donasi.js
@@ -0,0 +1,19 @@
+let handler = async m => m.reply(`
+
+╭─「 Donasi • Pulsa 」
+
+│ •  [0852-1411-0608]
+
+│ •  [Owner Number]
+
+╰────
+
+`.trim()) // Tambah sendiri kalo mau
+
+handler.help = ['donasi']
+
+handler.tags = ['info']
+
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
